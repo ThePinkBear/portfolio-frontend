2349468eada76488e195336812d1aa1e5b17703a
Björn Noctiluca <bjorn.noctiluca@outlook.com>
2023-03-30 21:47:34 +0200

    Test changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,23 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Björn Noctiluca</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <h2>
+          .Net Fullstack developer
+        </h2>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        This portfolio page is under construction, check back later!
       </p>
     </div>
   )
