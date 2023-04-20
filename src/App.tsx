5cbac7b82f8e7a7c5a02592745bec6c6c18c3afc
Björn Noctiluca <bjorn.noctiluca@outlook.com>
2023-04-20 14:07:39 +0200

    Fetching from backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
+interface Item {
+  id: number;
+  text: string;
+}
+
 function App() {
+
+  const [myApi, setMyApi] = useState<Item[]>([]);
+
+  useEffect(() => {
+    fetch("https://pinkbear-portfolio-backend.azurewebsites.net/portfoliobackend")
+    .then(response => response.json())
+    .then(data => setMyApi(data));
+  }, []);
 
   return (
     <div className="App">
@@ -19,6 +33,13 @@ function App() {
       <p className="read-the-docs">
         This portfolio page is under construction, check back later!
       </p>
+      <div>
+        {myApi.map((item: Item) => (
+          <div key={item.id}>
+            <h3>{item.text}</h3>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
