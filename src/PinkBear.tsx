32a0a1e6059bd5e69ca99cc5b01de8a6587a23a2
Björn Noctiluca <bjorn.noctiluca@outlook.com>
2023-05-09 13:07:43 +0200

    Move image url fetch to backend

--- src/PinkBear.tsx
+++ src/PinkBear.tsx
@@ -2,42 +2,49 @@ import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './App.css';
 
-interface Item {
+interface TextPost {
   id: number;
   name: string;
   text: string;
 }
 
 function PinkBear() {
-  const [myApi, setMyApi] = useState<Item[] >([]);
+  const [textPost, setTextPost] = useState<TextPost[] >([]);
+  const [image, setImage] = useState<string>('');
   const API_URL = (import.meta.env.VITE_API_URL as string);
-  const IMAGE_URL : string = `https://mknawqiahttnfewbzfzq.supabase.co/storage/v1/object/sign/portfolio-images-pinkbear/pp_bw.JPG?token=${import.meta.env.VITE_API_BUCKET_TOKEN}`;
+  
   useEffect(() => {
     const fetchData = () => {
       fetch(`https://${API_URL}/api/texts/test`)
         .then(response => response.json())
-        .then(data => setMyApi(data));
+        .then(data => setTextPost(data));
+    }
+    const fetchPp = () => {
+      fetch(`https://${API_URL}/api/portfoliobackend/profile-pictures?image=pp_bw.JPG`)
+        .then(response => response.text())
+        .then(data => setImage(data as string));
     }
     fetchData();
-  }, []);    
+    fetchPp();
+  }, []);
   return (
     <main>
-      <img src={`${IMAGE_URL}`} className="image" alt="A picture of me." />
+      <img src={`${image}`} className="image" alt="A picture of me." />
       <h1>Björn Noctiluca</h1>
-        <h2>.Net Fullstack developer</h2>
-          <p className="read-the-docs">
-            More <Link to="/about">about me</Link> and my <Link to="/projects">projects</Link> coming soon! <br />
-          </p>
-          <p>can we reach the backend?:</p>
-            {
-              myApi.map(item => (
-              <div key={item.id}>
-                <h2>{item.name}</h2>
-                <h2>{item.text}</h2>
-              </div>
-              ))
-            } 
-      </main>
+      <h2>.Net Fullstack developer</h2>
+        <p className="read-the-docs">
+          More <Link to="/about">about me</Link> and my <Link to="/projects">projects</Link> coming soon! <br />
+        </p>
+        <p>can we reach the backend?:</p>
+          {
+            textPost.map(item => (
+            <div key={item.id}>
+              <h2>{item.name}</h2>
+              <h2>{item.text}</h2>
+            </div>
+            ))
+          } 
+    </main>
   )
 }
 
