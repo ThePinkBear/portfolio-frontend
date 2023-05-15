6284e8059bc911bea619a3331b22b50fa5b8a793
Björn Noctiluca <bjorn.noctiluca@outlook.com>
2023-05-15 16:31:29 +0200

    No really I fixed it...

--- src/About.tsx
+++ src/About.tsx
@@ -1,16 +1,12 @@
-import { Link } from 'react-router-dom'
-import './App.css'
-import './PinkBear.css'
 import { useState, useEffect } from 'react'
-import { TextPost } from './Interfaces'
+import { Link } from 'react-router-dom'
+import { TextPost } from './Interfaces';
+import './PinkBear.css'
+import './App.css'
 
 const About = () => {
-
   const [textPost, setTextPost] = useState<TextPost[] >([]);
-  const [abouts, setAbouts] = useState<string[] >([]);
   const API_URL = (import.meta.env.VITE_API_URL as string);
-  const profile = textPost.find(tp => tp.name === "about")?.text.split('\n') as string[];
-  
   
   const fetchData = () => {
     fetch(`https://${API_URL}/api/texts/test`)
@@ -19,26 +15,19 @@ const About = () => {
   }
   
   useEffect(() => {
-    if (!textPost.length){
-      fetchData();
-      setAbouts(profile);
-    }
+    fetchData();
   }, []);
+  const about = textPost.find(tp => tp.name === "about")?.text.split('\n') as string[];
+
+  if(!about) return <h1>loading...</h1>
 
   return (
     <>
-      <article className='pinkBear-content'>
+      <section className='pinkBear-content'>
         <Link to="/">back</Link>
         <h3>About me:</h3>
-        <p>
-          { abouts ?
-          abouts.map((about, index) => (
-            <span key={index}>{about}<br/></span> 
-          ))
-          :
-          <h1>Loading...</h1>}
-        </p>
-      </article>
+          {about.map((about, index) => (<p key={index}>{about}<br/></p>))}
+      </section>
     </>
   )
 }
